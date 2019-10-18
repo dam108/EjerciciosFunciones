ce95d12fa9412f01d33ab6c272fefb4f2006784a
dam108 <dam108@A20E926.wirtz.local>
2019-10-18 11:32:28 +0200

    desde clase 18/10/2019

--- src/ejerciciosfunciones/EjerciciosFunciones.java
+++ src/ejerciciosfunciones/EjerciciosFunciones.java
@@ -10,12 +10,13 @@ package ejerciciosfunciones;
  * @author dam108
  */
 public class EjerciciosFunciones {
-
+ 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hola mundo");
     }
     
 }
