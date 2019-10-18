769eb76bcf486de79ddedcd06fdaa82c225f49c9
dam108 <dam108@A20E926.wirtz.local>
2019-10-18 11:36:32 +0200

    desde clase 18/10/2019

---
+++ src/ejerciciosfunciones/Esqueleto.java
@@ -0,0 +1,9 @@
+package ejerciciosfunciones;
+import java.util.Scanner;
+public class Esqueleto {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+    }
+    
+}
---
+++ src/ejerciciosfunciones/prueba.java
@@ -0,0 +1,21 @@
+package ejerciciosfunciones;
+import java.util.Scanner;
+public class prueba {
+    public static void main (String [] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduce una cadena:");
+        // retiramos la string cadena = teclado.nextLine(); por que no nos hace falta, ya grabamos la cadena directamente en la variable cadenaSB
+        StringBuilder cadenaSB = new StringBuilder(teclado.nextLine());
+        int pos;
+
+        for (int i = 0; i < cadenaSB.length(); i++){
+            pos = cadenaSB.indexOf(" ");
+            if (pos !=-1) {
+                cadenaSB.deleteCharAt(pos);
+            }
+        }
+
+        System.out.println(cadenaSB);
+
+    } // fin main
+} // fin class
